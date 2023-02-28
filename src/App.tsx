282d7d38b1e812b0ede91ae2b618d0a442e62e37
Petryk Dmytro <dimapetr2002@gmail.com>
2023-02-28 20:37:33 +0200

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -6,43 +6,22 @@ import { PageNotFound } from './pages/PageNotFound/PageNotFound';
 import { Phones } from './pages/Phones/Phones';
 import { Tablets } from './pages/Tablets/Tablets';
 import { Accessories } from './pages/Accessories/Accessories';
-
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+import { Header } from './components/Header/Header';
 
 export const App: React.FC = () => {
   return (
-    <>
-      <div className="starter">
-        <Provider onClick={() => ({})}>
-          { /* <TodoList /> */ }
-        </Provider>
+    <div className="section">
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Navigate to="/" replace />} />
+          <Route path="/phones" element={<Phones />} />
+          <Route path="/tablets" element={<Tablets />} />
+          <Route path="/accessories" element={<Accessories />} />
+        </Routes>
       </div>
-
-      <div className="section">
-        <div className="container">
-          <Routes>
-            <Route path="*" element={<PageNotFound />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Navigate to="/" replace />} />
-            <Route path="/phones" element={<Phones />} />
-            <Route path="/tablets" element={<Tablets />} />
-            <Route path="/accessories" element={<Accessories />} />
-          </Routes>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
