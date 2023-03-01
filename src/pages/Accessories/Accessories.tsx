0263c9db37d05c5afa003d1ad9d1d5908f11cda6
daryna-zinchenko <zinchenko.daryna@gmail.com>
2023-03-01 10:17:01 +0100

    grid_catalog

--- src/pages/Accessories/Accessories.tsx
+++ src/pages/Accessories/Accessories.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Catalog } from '../../components/catalog/Catalog';
 import './Accessories.scss';
 
 export const Accessories = () => {
+  const productCards = [
+    { name: 'Product card', id: 1 },
+    { name: 'Product card', id: 2 },
+    { name: 'Product card', id: 3 },
+    { name: 'Product card', id: 4 },
+    { name: 'Product card', id: 5 },
+    { name: 'Product card', id: 6 },
+    { name: 'Product card', id: 7 },
+    { name: 'Product card', id: 8 },
+    { name: 'Product card', id: 9 },
+    { name: 'Product card', id: 10 },
+  ];
+
   return (
-    <div className="container">
-      <h1>Accessories</h1>
-    </div>
+    <Catalog
+      products={productCards}
+      title="Accessories"
+    >
+    </Catalog>
   );
 };
