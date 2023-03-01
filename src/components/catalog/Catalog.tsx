a067b8b584aabc15192633e5b349d380201cc29c
daryna-zinchenko <zinchenko.daryna@gmail.com>
2023-03-01 15:33:30 +0100

    grid_catalog fixed paddings

--- src/components/catalog/Catalog.tsx
+++ src/components/catalog/Catalog.tsx
@@ -12,26 +12,24 @@ type Props = {
 export const Catalog: React.FC<Props> = ({ products, title }) => {
   return (
     <main className="catalog">
-      <div className="wrapper">
-        <h1 className="catalog__category">
-          {title}
-        </h1>
-        <p className="catalog__quantity">{`${products.length} models`}</p>
+      <h1 className="catalog__category">
+        {title}
+      </h1>
+      <p className="catalog__quantity">{`${products.length} models`}</p>
 
-        <select name="sort" className="catalog__sort">
-          <option value="Newest">Newest</option>
-          <option value="Oldest">Oldest</option>
-          <option value="Cheapest">Cheapest</option>
-          <option value="Cheapest">Most expensive</option>
-        </select>
+      <select name="sort" className="catalog__sort">
+        <option value="Newest">Newest</option>
+        <option value="Oldest">Oldest</option>
+        <option value="Cheapest">Cheapest</option>
+        <option value="Cheapest">Most expensive</option>
+      </select>
 
-        <div className="catalog__list">
-          {products.map(product => (
-            <div className="catalog__card" key={product.id}>
-              {product.name}
-            </div>
-          ))}
-        </div>
+      <div className="catalog__list">
+        {products.map(product => (
+          <div className="catalog__card" key={product.id}>
+            {product.name}
+          </div>
+        ))}
       </div>
     </main>
   );
