207c47f120a2fb300c52fd9c4e1522c00ecf0864
Vasya Rymarenko <vrima60@gmail.com>
2023-03-01 19:02:36 +0200

    Card removed from main page

--- src/App.tsx
+++ src/App.tsx
@@ -10,30 +10,11 @@ import { Accessories } from './pages/Accessories/Accessories';
 import { Header } from './components/Header/Header';
 import { Contacts } from './pages/Contacts/Contacts';
 import { Rights } from './pages/Rights/Rights';
-import { Card } from './components/Card/Card';
-
-const phoneFromServer = {
-  id: '1',
-  category: 'phones',
-  phoneId: 'apple-iphone-7-32gb-black',
-  itemId: 'apple-iphone-7-32gb-black',
-  name: 'Apple iPhone 7 32GB Black',
-  fullPrice: 400,
-  price: 375,
-  screen: "4.7' IPS",
-  capacity: '32GB',
-  color: 'black',
-  ram: '2GB',
-  year: 2016,
-  image: 'img/phones/apple-iphone-7/black/00.jpg',
-};
 
 export const App: React.FC = () => {
   return (
     <div className="section">
       <Header />
-
-      <Card phone={phoneFromServer} />
 
       <div className="container">
         <Routes>
