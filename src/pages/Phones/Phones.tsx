34233d7b564a71d8db2afc163c74d654010fb131
daryna-zinchenko <zinchenko.daryna@gmail.com>
2023-03-01 01:32:19 +0100

    grid_catalog

--- src/pages/Phones/Phones.tsx
+++ src/pages/Phones/Phones.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Catalog } from '../../components/catalog/Catalog';
 import './Phones.scss';
 
 export const Phones = () => {
+  const productCards = [
+    { name: 'Product card', id: 1 },
+    { name: 'Product card', id: 2 },
+    { name: 'Product card', id: 3 },
+    { name: 'Product card', id: 4 },
+    { name: 'Product card', id: 5 },
+    { name: 'Product card', id: 6 },
+    { name: 'Product card', id: 7 },
+    { name: 'Product card', id: 8 },
+    { name: 'Product card', id: 9 },
+    { name: 'Product card', id: 10 },
+  ];
+
   return (
-    <div className="container">
-      <h1>Phones</h1>
-    </div>
+    <Catalog
+      products={productCards}
+      title="Mobile phones"
+    >
+    </Catalog>
   );
 };
