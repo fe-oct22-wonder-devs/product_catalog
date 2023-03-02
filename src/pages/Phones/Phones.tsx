0b208ba666825e2a9ccdd0735b7cfccccad6ca86
Vasya Rymarenko <vrima60@gmail.com>
2023-03-02 11:17:01 +0200

    card added to catalog

--- src/pages/Phones/Phones.tsx
+++ src/pages/Phones/Phones.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Catalog } from '../../components/catalog/Catalog';
 import './Phones.scss';
+import phonesFromServer from '../../data/phones.json';
 
 export const Phones = () => {
-  const productCards = [
-    { name: 'Product card', id: 1 },
-    { name: 'Product card', id: 2 },
-    { name: 'Product card', id: 3 },
-    { name: 'Product card', id: 4 },
-    { name: 'Product card', id: 5 },
-    { name: 'Product card', id: 6 },
-    { name: 'Product card', id: 7 },
-    { name: 'Product card', id: 8 },
-    { name: 'Product card', id: 9 },
-    { name: 'Product card', id: 10 },
-  ];
+  // const productCards = [
+  //   { name: 'Product card', id: 1 },
+  //   { name: 'Product card', id: 2 },
+  //   { name: 'Product card', id: 3 },
+  //   { name: 'Product card', id: 4 },
+  //   { name: 'Product card', id: 5 },
+  //   { name: 'Product card', id: 6 },
+  //   { name: 'Product card', id: 7 },
+  //   { name: 'Product card', id: 8 },
+  //   { name: 'Product card', id: 9 },
+  //   { name: 'Product card', id: 10 },
+  // ];
 
   return (
     <Catalog
-      products={productCards}
+      products={phonesFromServer}
       title="Mobie phones"
     >
     </Catalog>
