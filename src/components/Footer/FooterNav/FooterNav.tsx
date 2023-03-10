87414d852ddacdb08085ed588c1b3d181aba5b93
Vasya Rymarenko <vrima60@gmail.com>
2023-03-10 16:48:58 +0200

    contacts link fixed

--- src/components/Footer/FooterNav/FooterNav.tsx
+++ src/components/Footer/FooterNav/FooterNav.tsx
@@ -5,7 +5,7 @@ export const FooterNav: React.FC = React.memo(() => {
   return (
     <div className="footer__nav">
       <FooterNavLink to="https://github.com/orgs/fe-oct22-wonder-devs/repositories?type=source" linkTitle="GITHUB" />
-      <FooterNavLink to="/https://github.com/orgs/fe-oct22-wonder-devs/people" linkTitle="CONTACTS" />
+      <FooterNavLink to="https://github.com/orgs/fe-oct22-wonder-devs/people" linkTitle="CONTACTS" />
       <FooterNavLink to="/rights" linkTitle="RIGHTS" />
     </div>
   );
